7767e2fceb83dd26ee3316b725c1ffee9cbf6c7d
bryn-ellison <bryn.ellison@outlook.com>
2024-09-10 15:10:34 +0100

    add typeEffect functionality

--- src/SignOff.jsx
+++ src/SignOff.jsx
@@ -1,11 +1,14 @@
 import { useState, useEffect } from 'react'
 import { fetcherWithAxios } from './FetchWithAxios';
+import Typewriter from './TypeEffect';
+
 
 const SignOff = () => {
 
     const [signOffData, setSignOffData] = useState(null);
     const [loading, setLoading] = useState(true);
     const [error, setError] = useState(null);
+    const [finishedTyping, setFinishedTyping] = useState(false);
   
     useEffect(() => {
       const fetchSignOffData = async () => {
@@ -24,16 +27,26 @@ const SignOff = () => {
       };
       fetchSignOffData();
     }, []);
-  
+    
+    const handleFinishedtyping = (bool) => {  
+        setFinishedTyping(bool)  
+    }  
+
+    const DisplayAuthor = () => {      
+        if (finishedTyping === false) {
+            return
+        } else {
+            return <Typewriter text= {signOffData.author} speed={40} handleFinishedtyping={handleFinishedtyping}/>
+        }
+    };
+
     return (
     <>
         <div>
             {loading && (<div>Loading...</div>)}
-            {error && <div>{error}</div>}
-            <div>
-                <h2>{signOffData?.signOff}</h2>
-                <p>{signOffData?.author}</p>
-            </div>
+            {error && (<div>{error}</div>)}
+            {signOffData && (<Typewriter text={signOffData.signOff} speed={40} handleFinishedtyping={handleFinishedtyping}/>)}
+            <DisplayAuthor />
         </div>
     </>
     );
---
+++ src/TypeEffect.jsx
@@ -0,0 +1,32 @@
+import  { useState, useEffect } from 'react';
+
+const typeEffect = (text = "", speed = 50, handleFinishedtyping) => {
+  const [displayText, setDisplayText] = useState('');
+  
+  useEffect(() => {
+    let i = 0;
+    const typingInterval = setInterval(() => {
+      if (i < text.length) {
+        setDisplayText(text.substring(0, i + 1))
+        i++;
+      } else {
+        handleFinishedtyping(true)
+        clearInterval(typingInterval);
+      }
+    }, speed);
+
+    return () => {
+      clearInterval(typingInterval);
+      
+    };
+  }, [text, speed]);
+
+  return displayText;
+};
+
+const UseTypeEffect = ({ text, speed, handleFinishedtyping }) => {
+    const displayText = typeEffect(text, speed, handleFinishedtyping);
+    return <p>{displayText}</p>;
+  };
+  
+export default UseTypeEffect;
