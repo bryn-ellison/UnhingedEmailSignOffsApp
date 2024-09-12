04913c96001f6ed40dad4747560ab34b44bfcb54
bryn-ellison <bryn.ellison@outlook.com>
2024-09-12 17:40:09 +0100

    amend Auth0 router setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,34 +5,44 @@ import CreateSignOff from "./CreateSignOff.jsx";
 import ErrorPage from "./error-page";
 import Admin from "./Admin";
 import "./index.css";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/create",
-    element: <CreateSignOff />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: "/admin",
-    element: <Admin />,
-    errorElement: <ErrorPage />,
-  },
-]);
-
-createRoot(document.getElementById("root")).render(
+const Auth0ProviderLayout = () => (
   <Auth0Provider
     domain="dev-pc2rdn4i8ffin0d4.uk.auth0.com"
     clientId="yBHkadJfpzGsft7alJBqteh78Uj5UEMW"
     authorizationParams={{
-      redirect_uri: "/admin",
+      redirect_uri: window.location.origin,
     }}
   >
-    <RouterProvider router={router} />
+    <Outlet />
   </Auth0Provider>
+);
+
+const router = createBrowserRouter([
+  {
+    element: <Auth0ProviderLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <App />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/create",
+        element: <CreateSignOff />,
+        errorElement: <ErrorPage />,
+      },
+      {
+        path: "/admin",
+        element: <Admin />,
+        errorElement: <ErrorPage />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+  <RouterProvider router={router} />
 );
