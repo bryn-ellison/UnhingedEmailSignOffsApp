d276745b58de0677fa26095dfaa983cc2a638610
bryn-ellison <bryn.ellison@outlook.com>
2024-09-12 17:59:57 +0100

    amend route setup

--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,13 @@ import CreateSignOff from "./CreateSignOff.jsx";
 import ErrorPage from "./error-page";
 import Admin from "./Admin";
 import "./index.css";
-import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Route,
+} from "react-router-dom";
 
 const Auth0ProviderLayout = () => (
   <Auth0Provider
@@ -19,29 +25,47 @@ const Auth0ProviderLayout = () => (
   </Auth0Provider>
 );
 
-const router = createBrowserRouter([
-  {
-    element: <Auth0ProviderLayout />,
-    errorElement: <ErrorPage />,
-    children: [
-      {
-        path: "/",
-        element: <App />,
-        errorElement: <ErrorPage />,
-      },
-      {
-        path: "/create",
-        element: <CreateSignOff />,
-        errorElement: <ErrorPage />,
-      },
-      {
-        path: "/admin",
-        element: <Admin />,
-        errorElement: <ErrorPage />,
-      },
-    ],
-  },
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route element={<Auth0ProviderLayout />}>
+      <Route path="/" element={<App />} errorElement={<ErrorPage />}></Route>
+      <Route
+        path="/create"
+        element={<CreateSignOff />}
+        errorElement={<ErrorPage />}
+      ></Route>
+      <Route
+        path="/admin"
+        element={<Admin />}
+        errorElement={<ErrorPage />}
+      ></Route>
+    </Route>
+  )
+);
+
+// const router = createBrowserRouter([
+//   {
+//     element: <Auth0ProviderLayout />,
+//     errorElement: <ErrorPage />,
+//     children: [
+//       {
+//         path: "/",
+//         element: <App />,
+//         errorElement: <ErrorPage />,
+//       },
+//       {
+//         path: "/create",
+//         element: <CreateSignOff />,
+//         errorElement: <ErrorPage />,
+//       },
+//       {
+//         path: "/admin",
+//         element: <Admin />,
+//         errorElement: <ErrorPage />,
+//       },
+//     ],
+//   },
+// ]);
 
 createRoot(document.getElementById("root")).render(
   <RouterProvider router={router} />
