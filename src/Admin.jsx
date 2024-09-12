f10ec8300ba1e17b67259d56f666ae0b689c4ed5
bryn-ellison <bryn.ellison@outlook.com>
2024-09-12 19:03:22 +0100

    add user profile

--- src/Admin.jsx
+++ src/Admin.jsx
@@ -1,10 +1,32 @@
 import LoginButton from "./LoginButton";
+import LogoutButton from "./LogoutButton";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const adminArea = () => {
+  const Profile = () => {
+    const { user, isAuthenticated, isLoading } = useAuth0();
+
+    if (isLoading) {
+      return <div>Loading ...</div>;
+    }
+
+    return (
+      isAuthenticated && (
+        <div>
+          <img src={user.picture} alt={user.name} />
+          <h2>{user.name}</h2>
+          <p>{user.email}</p>
+        </div>
+      )
+    );
+  };
+
   return (
     <div>
       <h1>Admin Area</h1>
+      <Profile />
       <LoginButton />
+      <LogoutButton />
     </div>
   );
 };
