bb6f14fec5e90b18ed0003c3e230236de3d0b68b
bryn-ellison <bryn.ellison@outlook.com>
2024-09-13 15:28:00 +0100

    separate user login and profile

--- src/AdminArea.jsx
+++ src/AdminArea.jsx
@@ -2,59 +2,30 @@ import LoginButton from "./LoginButton";
 import LogoutButton from "./LogoutButton";
 import { useAuth0 } from "@auth0/auth0-react";
 import { useState, useEffect } from "react";
+import Profile from "./Profile";
 
 const AdminArea = () => {
-  const { user, isAuthenticated, getAccessTokenSilently } = useAuth0();
-  const [userMetadata, setUserMetadata] = useState(null);
-  console.log(user.sub + " USER SUB???");
-  useEffect(() => {
-    const getUserMetadata = async () => {
-      const domain = "dev-pc2rdn4i8ffin0d4.uk.auth0.com";
+  const { user, isAuthenticated, isLoading } = useAuth0();
 
-      try {
-        const accessToken = await getAccessTokenSilently({
-          authorizationParams: {
-            audience: "UnhingedEmailSignOffsApi",
-            scope: "read:signoffs write:signoffs update:signoffs",
-          },
-        });
+  const CheckLogin = () => {
+    if (isLoading) {
+      return <div>Loading ...</div>;
+    }
 
-        const userDetailsByIdUrl = `https://${domain}/api/v2/users/${user.sub}`;
-
-        const metadataResponse = await fetch(userDetailsByIdUrl, {
-          headers: {
-            Authorization: `Bearer ${accessToken}`,
-          },
-        });
-
-        const { user_metadata } = await metadataResponse.json();
-        console.log(user_metadata + " IS USER METADATA Working?");
-        setUserMetadata(user_metadata);
-      } catch (e) {
-        console.log(e.message);
-      }
-    };
-
-    getUserMetadata();
-  }, [getAccessTokenSilently, user?.sub]);
+    return (
+      isAuthenticated && (
+        <div>
+          <Profile />
+          <LogoutButton />
+        </div>
+      )
+    );
+  };
 
   return (
     <div>
       <h1>Admin Area</h1>
-      {isAuthenticated && (
-        <div>
-          <img src={user.picture} alt={user.name} />
-          <h2>{user.name}</h2>
-          <p>{user.email}</p>
-          <h3>User Metadata</h3>
-          {userMetadata ? (
-            <pre>{JSON.stringify(userMetadata, null, 2)}</pre>
-          ) : (
-            "No user metadata defined"
-          )}
-          <LogoutButton />
-        </div>
-      )}
+      <CheckLogin />
       <LoginButton />
     </div>
   );
