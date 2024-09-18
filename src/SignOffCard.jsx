7cc1ab9f2e8a2e1414e26540d77b0df3c9412fa2
bryn-ellison <bryn.ellison@outlook.com>
2024-09-18 16:07:28 +0100

    fix iteration attempt 12

--- src/SignOffCard.jsx
+++ src/SignOffCard.jsx
@@ -41,7 +41,7 @@ const SignOffCard = ({ signOff, listView }) => {
   };
 
   async function handleAdminTaskButtonClick(buttonText) {
-    console.log("FIRES");
+    return console.log("FIRES");
     // try {
     //   const token = await getAccessTokenSilently({
     //     authorizationParams: {
