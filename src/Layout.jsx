dc07e3444c08dfe9ac120817561eb8b75980d6e6
bryn-ellison <bryn.ellison@outlook.com>
2024-09-26 18:58:13 +0100

    styling added

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -1,11 +1,19 @@
 import React from "react";
 import { Outlet } from "react-router-dom";
+import { SlEnvolope, SlPaperPlane } from "react-icons/sl";
 
 function Layout() {
   return (
     <div className="content-wrapper">
       <header>
-        <h1>Unhinged Email Sign Offs</h1>
+        <div className="top-icon-container">
+          <SlEnvolope />
+          <SlPaperPlane />
+        </div>
+        <div className="title-container">
+          <div className="to-button">To:</div>
+          <h1>Unhinged Email Sign Offs</h1>
+        </div>
       </header>
       <main>
         <Outlet />
