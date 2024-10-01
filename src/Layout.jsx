f75566fc8b679b0ab79fcd673ac12990c6965f9b
bryn-ellison <bryn.ellison@outlook.com>
2024-10-01 13:27:33 +0100

    add footer

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -2,23 +2,32 @@ import React from "react";
 import { Outlet } from "react-router-dom";
 import { SlEnvolope, SlPaperPlane } from "react-icons/sl";
 
+const year = new Date().getFullYear();
+
 function Layout() {
   return (
-    <div className="content-wrapper">
-      <header>
-        <div className="top-icon-container">
-          <SlEnvolope />
-          <SlPaperPlane />
-        </div>
-        <div className="title-container">
-          <div className="to-button">To:</div>
-          <h1>Unhinged Email Sign Offs</h1>
-        </div>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-    </div>
+    <>
+      <div className="content-wrapper">
+        <header>
+          <div className="top-icon-container">
+            <SlEnvolope />
+            <SlPaperPlane />
+          </div>
+          <div className="title-container">
+            <div className="to-button">To:</div>
+            <h1>Unhinged Email Sign Offs</h1>
+          </div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </div>
+      <footer>
+        <a href="https://github.com/bryn-ellison">©{year} Bryn Ellison</a>
+        <p>|</p>
+        <a href="https://www.linkedin.com/in/brynellison/">Hire me?</a>
+      </footer>
+    </>
   );
 }
 
