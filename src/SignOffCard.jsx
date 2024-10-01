cb679be5fc8155edc04897ad870d9ad078c68ac9
bryn-ellison <bryn.ellison@outlook.com>
2024-10-01 15:23:05 +0100

    fix admin card styles

--- src/SignOffCard.jsx
+++ src/SignOffCard.jsx
@@ -90,7 +90,7 @@ const SignOffCard = ({ handleTaskButtonClick, signOff, listView }) => {
             value={signOff.author}
             onChange={handleChange}
           />
-          <div className="admin-buttons-container">
+          <div className="buttons-container">
             <button className="ui-btn" form="submit-edit-form" type="submit">
               Submit
             </button>
